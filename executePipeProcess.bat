36161add5ea091480492648f2aea04131560c02c
kevinl03 <klitvin101@gmail.com>
2024-03-07 15:15:50 -0800

    added relative pathing so directories can be found independant of batch file execution

--- executePipeProcess.bat
+++ executePipeProcess.bat
@@ -4,7 +4,7 @@ rem Navigate to the source directory
 gcc %~dp0archive\PipeCom.C -o %~dp0archive\pipe.exe -lws2_32
 
 rem Navigate to the output directory
-cd archive
+cd %~dp0archive
 
 rem Execute pipe receival from firmware
-pipe.exe
+.\pipe.exe
